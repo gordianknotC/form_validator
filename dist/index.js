27f8a47d63892f68cb5d540732a5817d9c6e0353
curtis <curtis@fillygaming888.com>
2022-06-01 00:56:15 +0800

    Add getValidationRules

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,6 @@ import { VForm, } from "~/base/vformTypes";
  * */
 import { BaseFormContext, BaseFormImpl, BaseFormModel, } from "~/base/baseFormImpl";
 import { createFormState, HiddenField, FormField } from "~/base/formStateUtil";
-import { EBaseValidationRules, getFormRules, addRule } from "~/base/formRuleUtil";
-export { BaseFormImpl, BaseFormModel, BaseFormContext, EBaseValidationRules, VForm, createFormState, HiddenField, FormField, addRule, getFormRules };
+import { EBaseValidationRules, getValidationRules, addRule, getFormRules } from "~/base/formRuleUtil";
+export { BaseFormImpl, BaseFormModel, BaseFormContext, EBaseValidationRules, VForm, createFormState, HiddenField, FormField, addRule, getValidationRules, getFormRules };
 //# sourceMappingURL=index.js.map
