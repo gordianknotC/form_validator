5405c56331169a8a59a6e868df93981fbc720683
curtis <curtis@fillygaming888.com>
2022-06-01 01:29:41 +0800

    Add export type

--- src/index.ts
+++ src/index.ts
@@ -13,13 +13,10 @@ import {BaseFormContext, BaseFormImpl, BaseFormModel, } from "~/base/baseFormImp
 import {createFormState, HiddenField, FormField} from "~/base/formStateUtil";
 import {EBaseValidationRules, baseValidationRules, getValidationRules, addRule, getFormRules, DefaultFormRules, DefaultValidationRules} from "~/base/formRuleUtil";
 export {
-  DefaultFormRules,
-  DefaultValidationRules,
   BaseFormImpl,
   BaseFormModel,
   BaseFormContext,
   EBaseValidationRules,
-  VForm,
   createFormState,
   HiddenField,
   FormField,
@@ -28,3 +25,8 @@ export {
   getFormRules
 }
 
+export type{
+  VForm,
+  DefaultFormRules,
+  DefaultValidationRules,
+}
