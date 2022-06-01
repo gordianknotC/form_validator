edbe2524ca96bfbe6d321b9b1a33b581c071d23e
curtis <curtis@fillygaming888.com>
2022-06-01 12:43:36 +0800

    Update packages

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,6 @@
 export type { VForm } from "~/base/vformTypes";
 export type { DefaultFieldRules, DefaultValidationRules } from "~/base/formRuleUtil";
+export {} from "common_js_builtin";
 /**
  *        M I X I N S
  *
@@ -8,3 +9,24 @@ export type { DefaultFieldRules, DefaultValidationRules } from "~/base/formRuleU
 export { BaseFormContext, BaseFormImpl, BaseFormModel, } from "~/base/baseFormImpl";
 export { createFormState, HiddenField, FormField, } from "~/base/formStateUtil";
 export { EBaseValidationRules, baseValidationRules, getValidationRules, addValidationRule, getFormRules, addFieldRule } from "~/base/formRuleUtil";
+/**
+ *      B A S E
+ *
+ * */
+export { UnCaughtCondition, UncaughtEnumType, NotImplementedError, TypeMismatchError, InvalidUsage, UnExpectedRole } from "common_js_builtin";
+/***
+ *
+ *    E X T E N D
+ *    B A S E
+ * */
+export { AssertMsg, AssertionError, assert, } from "common_js_builtin";
+export { isRefImpl, asEnum, getAccessibleProperties, asCascadeClass, asUnWrappedVueRefMap, UnWrappedVueRef, addStringMappingFromNumEnum, Is, } from "common_js_builtin";
+export type { TUnWrapVueRef, InterfaceIs, } from "common_js_builtin";
+/**
+ *        M I X I N S
+ *
+ *
+ * */
+export { useBuiltIn } from "common_js_builtin";
+export { assertMsg } from "common_js_builtin";
+export { is } from "common_js_builtin";
--- dist/index.js
+++ dist/index.js
@@ -6,4 +6,24 @@
 export { BaseFormContext, BaseFormImpl, BaseFormModel, } from "~/base/baseFormImpl";
 export { createFormState, HiddenField, FormField, } from "~/base/formStateUtil";
 export { EBaseValidationRules, baseValidationRules, getValidationRules, addValidationRule, getFormRules, addFieldRule } from "~/base/formRuleUtil";
+/**
+ *      B A S E
+ *
+ * */
+export { UnCaughtCondition, UncaughtEnumType, NotImplementedError, TypeMismatchError, InvalidUsage, UnExpectedRole } from "common_js_builtin";
+/***
+ *
+ *    E X T E N D
+ *    B A S E
+ * */
+export { AssertMsg, AssertionError, assert, } from "common_js_builtin";
+export { isRefImpl, asEnum, getAccessibleProperties, asCascadeClass, asUnWrappedVueRefMap, UnWrappedVueRef, addStringMappingFromNumEnum, Is, } from "common_js_builtin";
+/**
+ *        M I X I N S
+ *
+ *
+ * */
+export { useBuiltIn } from "common_js_builtin";
+export { assertMsg } from "common_js_builtin";
+export { is } from "common_js_builtin";
 //# sourceMappingURL=index.js.map
