a469123ccf125e2c7e568b8e3cac29bd971ca3eb
curtis <curtis@fillygaming888.com>
2022-06-01 12:43:05 +0800

    Update packages

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 export type { VForm } from "~/base/vformTypes"
 export type  { DefaultFieldRules, DefaultValidationRules} from "~/base/formRuleUtil";
 
+export {} from "common_js_builtin"
 
 /**
  *        M I X I N S
@@ -12,3 +13,30 @@ export {BaseFormContext, BaseFormImpl, BaseFormModel, } from "~/base/baseFormImp
 export {createFormState, HiddenField, FormField,} from "~/base/formStateUtil";
 export {EBaseValidationRules, baseValidationRules, getValidationRules, addValidationRule, getFormRules, addFieldRule} from "~/base/formRuleUtil";
 
+
+
+
+
+
+
+/**
+ *      B A S E
+ *
+ * */
+export { UnCaughtCondition, UncaughtEnumType, NotImplementedError, TypeMismatchError, InvalidUsage, UnExpectedRole } from "common_js_builtin";
+/***
+ *
+ *    E X T E N D
+ *    B A S E
+ * */
+export { AssertMsg, AssertionError, assert, } from "common_js_builtin";
+export { isRefImpl, asEnum, getAccessibleProperties, asCascadeClass, asUnWrappedVueRefMap, UnWrappedVueRef, addStringMappingFromNumEnum, Is, } from "common_js_builtin";
+export type { TUnWrapVueRef, InterfaceIs, } from "common_js_builtin";
+/**
+ *        M I X I N S
+ *
+ *
+ * */
+export { useBuiltIn } from "common_js_builtin";
+export { assertMsg } from "common_js_builtin";
+export { is } from "common_js_builtin";
